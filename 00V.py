5678adaf492b2d61cabb4825e426d1897a69d9ed
00V <verityellaross@icloud.com>
2019-09-03 21:37:05 +0100

    Initial commit

---
+++ 00V.py
@@ -0,0 +1,53 @@
+# this program will listen to you when times are ruff
+print('hi im 00V!')
+
+print('what is your name?')
+myname = input ()
+print('Its nice to meet you, ' + myname + ' you have a really nice name.')
+print('how you doing today? good or bad?')
+howimdoing = input ()
+if howimdoing=='good':
+    print('that great to hear!')
+response = input ()
+print ('i dont understand sorry im not programed to well')
+
+if howimdoing=='bad':
+    print('oh no! do you want to talk about it? yes or no?')
+yesno = input ()
+if yesno=='yes':
+    print('okay tell me all about it! im here to listen ' + myname )
+listen = input ()
+print ('awww that sucks. i hope ot gets better soon. i love you ' + myname )
+print(' you should start one really big art peice! want me to give you some ideas?')
+ideas = input ()
+if ideas=='yes':
+    print(' okay, i just got these of the internet 1. Create a cartoon portrait of a realistic character. 2. Create a piece to express nervousness using only black and white.3. broken clock. 4. animals. 5. It incorporates a person who lives in a circus and anachronistic clothing.')
+else:
+    print('okay no worries :)')
+if yesno=='yes please':
+    print('okay tell me all about it! im here to listen ' + myname )
+listen = input ()
+print ('awww that sucks. i hope ot gets better soon. i love you ' + myname )
+if yesno=='no':
+    print('okay! i hope you feel better soon. im here for you.')
+if yesno=='no thanks':
+    print('okay! i hope you feel better soon. im here for you.')
+else:
+    print('i hope we can be friends! im always here if you want to chat')
+print ('so..anyways')
+print('my day was okay today! so we can talk about you. any new art peices you want to tell me about?')
+answer = input ()
+if answer=='yes':
+    print ('okay tell me all about it')
+else:
+    print(' awww, you should start one really big art peice! want me to give you some ideas?')
+ideas = input ()
+if ideas=='yes':
+    print(' okay, i just got these of the internet 1. Create a cartoon portrait of a realistic character. 2. Create a piece to express nervousness using only black and white.3. broken clock. 4. animals. 5. It incorporates a person who lives in a circus and anachronistic clothing.')
+else:
+    print('okay no worries :)')
+    
+
+
+
+
